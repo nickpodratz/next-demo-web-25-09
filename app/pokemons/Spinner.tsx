54b2b2bcb12d28dd933d276d5571ce99be639938
Nick Podratz <nick.podratz@syntax-institut.de>
2026-07-16 12:02:20 +0200

    Fix tailwind syntax

--- app/pokemons/Spinner.tsx
+++ app/pokemons/Spinner.tsx
@@ -1,6 +1,6 @@
 export default function Spinner() {
     return (
-        <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
+        <div className="grid min-h-35 w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
             <svg
                 className="animate-spin text-gray-300"
                 viewBox="0 0 64 64"
