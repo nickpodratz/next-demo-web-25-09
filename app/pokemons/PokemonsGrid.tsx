732ca5be45dc16d8d22b9356ca8d3b726146c96b
Nick Podratz <nick.podratz@syntax-institut.de>
2026-07-16 10:56:13 +0200

    Add suspense boundary

---
+++ app/pokemons/PokemonsGrid.tsx
@@ -0,0 +1,20 @@
+import { type Pokemon } from "./types/Pokemon";
+
+export default async function PokemonGrid() {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+  const { results } = await response.json()
+  const pokemons: Pokemon[] = results ?? []
+
+  // Simuliere langsame UI.
+  await new Promise((resolve) => {
+    setTimeout(resolve, 4000);
+  });
+
+  return (
+    <div className="grid grid-cols-4">
+      {pokemons.map((p) => (
+        <div key={p.name} className="border border-black h-16 p-2">{p.name}</div>
+      ))}
+    </div>
+  );
+}
--- app/pokemons/page.tsx
+++ app/pokemons/page.tsx
@@ -1,20 +1,18 @@
+import { Suspense } from "react";
 import { type Pokemon } from "./types/Pokemon";
+import PokemonGrid from "./PokemonsGrid";
 
-export default async function Pokemon() {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon');
-  const { results } = await response.json()
-  const pokemons: Pokemon[] = results ?? []
-
-  // Simuliere langsame UI.
-  await new Promise((resolve) => {
-    setTimeout(resolve, 4000);
-  });
-
+export default async function Page() {
   return (
-    <div className="grid grid-cols-4">
-      {pokemons.map((p) => (
-        <div key={p.name} className="border border-black h-16 p-2">{p.name}</div>
-      ))}
-    </div>
-  );
+    <main className="border p-16">
+      <Suspense
+        fallback={
+          <p className="text-orange-600 text-2xl font-bold text-center">
+            Loading pokemon...
+          </p>
+        }
+      >
+        <PokemonGrid />
+      </Suspense>
+    </main>  );
 }
