c5c37f18945db43bc4ae8f867e5bc8959662e83a
Nick Podratz <nick.podratz@syntax-institut.de>
2026-07-24 11:18:24 +0200

    Add seed instead of deploy

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -4,8 +4,8 @@ set -e
 npx prisma generate
 
 if [ "$VERCEL_ENV" = "preview" ]; then
-  npx prisma migrate deploy
   npx prisma migrate reset --force
+  npx prisma db seed
 else
   npx prisma migrate deploy
 fi
